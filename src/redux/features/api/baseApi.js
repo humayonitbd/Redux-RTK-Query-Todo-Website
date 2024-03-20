3e96f4e1b85c1ba5c8423c15f68556fa4ec566f5
humayonitbd <humayonforid44@gmail.com>
2024-03-20 15:14:32 +0600

    base api setup

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,1 +1,10 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  endpoints: (builder) => ({
+    
+  }),
+})
+
