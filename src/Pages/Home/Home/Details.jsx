b92f48d1e3eda4b75fce71f914e1fb1c70ef96eb
humayonitbd <humayonforid44@gmail.com>
2024-03-21 16:45:55 +0600

    dinamic route create

--- src/Pages/Home/Home/Details.jsx
+++ src/Pages/Home/Home/Details.jsx
@@ -4,13 +4,16 @@ import { useGetPostByIdQuery } from '../../../redux/features/api/baseApi';
 import { useParams } from 'react-router-dom';
 
 const Details = () => {
-    const {id} = useParams()
-    const {data:item}= useGetPostByIdQuery(id)
+    const {id} = useParams();
+    console.log(id)
+    const {data:item}= useGetPostByIdQuery(id);
+    console.log(item)
     return (
-      <div className="my-10 mx-20">
+      <div className="my-20 mx-20">
           <div  className=" p-10 bg-zinc-600  mb-3">
-            <h3 className="text-xl mb-3 text-white ">{item.title}</h3>
-            <h3 className="text-white">{item.body}</h3>
+            <h3 className="text-xl mb-3 text-white ">{item?.title}</h3>
+            <h3 className="text-white">{item?.body}</h3>
+            {/* <h2>hi</h2> */}
           </div>
        
       </div>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../Pages/Home/Home/Home";
+import Details from "../Pages/Home/Home/Details";
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +10,10 @@ const routes = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: `/posts/:id`,
+        element: <Details />,
       },
       
     ],
