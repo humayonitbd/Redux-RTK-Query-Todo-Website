8c0d134a541abd6001888a85d80187d09a31d126
humayonitbd <humayonforid44@gmail.com>
2024-03-22 22:40:21 +0600

    code customize

--- src/Pages/Home/Home/Details.jsx
+++ src/Pages/Home/Home/Details.jsx
@@ -5,15 +5,14 @@ import { useParams } from 'react-router-dom';
 
 const Details = () => {
     const {id} = useParams();
-    console.log(id)
     const {data:item}= useGetPostByIdQuery(id);
-    console.log(item)
+    
     return (
       <div className="my-20 mx-20">
           <div  className=" p-10 bg-zinc-600  mb-3">
             <h3 className="text-xl mb-3 text-white ">{item?.title}</h3>
             <h3 className="text-white">{item?.body}</h3>
-            {/* <h2>hi</h2> */}
+          
           </div>
        
       </div>
